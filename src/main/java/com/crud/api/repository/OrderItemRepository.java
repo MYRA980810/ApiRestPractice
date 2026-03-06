0f824e02009ce498202b0f3779290b04da893d08
Christian Marquez <myramex23@gmail.com>
2026-03-05 21:31:09 -0700

    Agregando OrderItemRepository

--- src/main/java/com/crud/api/repository/OrderItemRepository.java
+++ src/main/java/com/crud/api/repository/OrderItemRepository.java
@@ -1,10 +1,18 @@
 package com.crud.api.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.crud.api.entity.OrderItem;
+
 // TODO 1: Crear interfaz que extienda JpaRepository<OrderItem, Long>
 
 // TODO 2: Definir métodos de consulta:
 //   - List<OrderItem> findByOrderId(Long orderId)
 //   - List<OrderItem> findByProductId(Long productId)
 
-public interface OrderItemRepository {
+public interface OrderItemRepository extends JpaRepository<Long, OrderItem> {
+    List<OrderItem> findByOrderId(Long id);
+    List<OrderItem> findByProductId(Long productId);
 }
